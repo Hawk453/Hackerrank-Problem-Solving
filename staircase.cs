ae4016a5f9d779b6ad4ecbbdcee042d2e67937b7
Hawk453 <sakshammadan@protonmail.com>
2020-12-28 16:13:38 +0530

    Challenges added

---
+++ staircase.cs
@@ -0,0 +1,23 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
+            Console.WriteLine(new String('#', i + 1).PadLeft(N, ' '));
+    }
+}
